e0ff950aaabffa143adb76db2f19ed99d2adacaf
AJ Mosley <ajmosley345@gmail.com>
2022-12-07 13:15:24 -0500

    Reorder and add controversy

--- pages/test.js
+++ pages/test.js
@@ -5,7 +5,6 @@ import { Typography, Divider } from '@mui/material';
 import Header from '../components/Header';
 import Image from 'next/image';
 import streets_of_rage from '../public/images/streets_of_rage.jpg';
-import barret_wallace from '../public/images/barret_wallace.webp';
 import barrett_old from '../public/images/barrett_old.png';
 import miles_morales from '../public/images/miles_morales.webp';
 import quartet from '../public/images/quartet.jpeg';
@@ -13,13 +12,15 @@ import punch_out from '../public/images/punch_out.jpeg';
 import tom_sawyer from '../public/images/tom_sawyer.png';
 import lee_everett from '../public/images/lee_everett.webp';
 import race_graph from '../public/images/race_graph.png';
+import marcus from '../public/images/marcus.webp';
+import chris from '../public/images/chris.webp';
 
 let timelineData = [
   {
     text: 'Mission Statement',
     date: '2022',
     image: '',
-    description: "The idea behind this project is to show how black representation has evolved in video games over the years. The timeline speaks for itself, but it begins as mostly racist or at least stereotypical representation, and slowly evolves to more nuanced characters that are more than just black. The connection between black aesthetics (Taylor), and video games is strong. I hope the timeline page brings that more to light. I make brief mention of the controversy that surrounds black characters, because it is another interesting avenue of thought. Since it's a break from the norm (white male), it brings people out of the woodwork that describe it as 'woke'. Hopefully this project is able to show the progress that has been made in black representation in video games, but also show that we have a long way to go in terms of both being good and acceptance.",
+    description: "The idea behind this project is to show how black representation has evolved in video games over the years. The timeline speaks for itself, but it begins as mostly racist or at least stereotypical representation, and slowly evolves to more nuanced characters that are more than just black. The connection between black aesthetics (Taylor), and video games is strong. I hope the timeline page brings that more to light. I make brief mention of the controversy that surrounds black characters, because it is another interesting avenue of thought. Since it's a break from the norm (white male), it brings people out of the woodwork that describe it as 'woke'. Hopefully this project is able to show the progress that has been made in black representation in video games, while also showing that there is a long way to go in making good black characters and the general acceptance within the community. ",
     alt: "",
     good: true
   },
@@ -27,7 +28,7 @@ let timelineData = [
     text: "1980's - Early 2000's",
     date: "1980's - Early 2000's",
     image: '',
-    description: "In the early days of video games, the representation of black people was very mixed, from what could be seen as good representation (Barret Wallace), to outright racist depictions (Jim from Square's Tom Sawyer). These games can be seen as major stepping stones, good and bad, for how black people will be represented.",
+    description: "In the early days of video games, the representation of black people was very mixed, from what could be seen as good representation (Barret Wallace), to outright racist depictions (Jim from Square's Tom Sawyer). These games can be seen as major stepping stones, good and bad, for how black people will be represented in video games during this time.",
   },
   {
     text: 'Quartet',
@@ -86,14 +87,6 @@ let timelineData = [
     good: true,
   },
   {
-    text: 'Diversity in Game Development',
-    date: "",
-    image: race_graph,
-    alt:"",
-    description: "Based on a survey conducted by the International Game Developers Association (IDGA), in 2019 about 2% of game developers were black. This makes sense in correlation to the lack of black protagonists in games as a whole." ,
-    good: true,
-  },
-  {
     text: 'The Walking Dead',
     date: '2012',
     image: lee_everett,
@@ -106,7 +99,47 @@ let timelineData = [
     date: '2020',
     image: miles_morales,
     alt:"",
-    description: 'Miles Morales is the main character, and while most of the time his face is covered, you play as him the whole game with only very subtle hints towards the broader social messages the game wants to send.',
+    description: 'Miles Morales, the main character, is an afro-latino living in New York. There are touches of ',
+    good: true,
+  },
+  {
+    text: 'Controversy Surrounding Black Representation in Games',
+    date: '2022',
+    image: '',
+    description: "It's hard to research the backlash to black representation in video games, as it gets combined with the claim of 'forced diversity' that has become increasingly prevalent in most forms of media. So I'm going to focus on a couple examples and hopefully it can still get the point across.",
+    alt: "",
+    good: true
+  },
+  {
+    text: 'Marcus Holloway (Watch Dogs 2)',
+    date: '2016',
+    image: marcus,
+    description: "During 2016 there were a lot of games that came out that featured black characters. One was Watch Dogs 2. The game touches on racial profiling, since it's set in a semi-future city with a lot of surveillance. The main controversy surrounding this was that some people would say that it was just Ubisoft (the game publisher) wanting to pander to 'Social Justice Warriors'. This was the first time I saw actual backlash against a black character, but it definitely wasn't the last. The constant yelling of 'pandering' or 'virtue signaling' when it comes to a non-white male being the center of a story is a tale as old as time at this point. ",
+    alt: "",
+    good: true
+  },
+  {
+    text: 'Resident Evil 5',
+    date: '2009',
+    image: chris,
+    description: "There isn't much to say about this game, it's set in Africa where you play as Chris Redfield killing a lot of Africans. Technically there is an in game reason, they're infected with a virus that makes them insane and attack you, but it still wasn't a good look. It actually had backlash against it in the fact that this was the 2nd time Capcom (the developer) had pulled this. This brings up the topic of japanese game developers being one of the worst offenders when it comes to black representation and stereotyping.",
+    alt: "",
+    good: true
+  },
+  {
+    text: 'Lack of Options in Customization',
+    date: 'Forever',
+    image: '',
+    description: "When it comes to RPGs/JRPGs there is always a severe lack of black hairstyles & facial features in their character creators. Whenever this is brought up, there is usually a small minority that likes to come out of the woodwork to push that criticism down. This obviously leads to poor representation of black people, because you can make the character's skin darker, but you can't actually represent what you want.",
+    alt: "",
+    good: true
+  },
+  {
+    text: 'Diversity in Game Development',
+    date: "",
+    image: race_graph,
+    alt:"",
+    description: "Based on a survey conducted by the International Game Developers Association (IDGA), in 2019 about 2% of game developers were black. This makes sense in correlation to the lack of black protagonists in games as a whole." ,
     good: true,
   },
 ]
